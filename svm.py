56371c1d9ae7f86d9d839c3df34f6d2ab2f94cc9
Aditya Khandkar <adiya.s.khandkar@gmail.com>
2019-10-30 14:53:39 -0400

    SVM model on custom dataset

--- svm.py
+++ svm.py
@@ -1,38 +1,42 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import preprocessing, svm
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn import svm
 from sklearn.model_selection import train_test_split as tts
 
-speed_data_aug = "C:/Aditya/College/Penn State/Semesters/5) Fall 2019/Master's Paper/Data sets/DataSet/speeddata_Aug.csv"
+speed_data_aug = "speeddata_Aug.csv"
+custom_data_aug = "customdata_Aug.csv"
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.expand_frame_repr', False)
+pd.set_option('max_colwidth', -1)
+
+df0 = pd.read_csv(speed_data_aug)
+df = pd.read_csv(custom_data_aug)
+df = df.drop(['Unnamed: 0'], axis=1)
+df['time_of_day'] = df0['time_id']
+
+print(df.head(150))
+
+df = df.dropna()
+
+X = df.iloc[:, 1:6] #.values.astype(float)
+y = df.iloc[:, 0:1] #.values.astype(float)
+
+# print(X[:10])
+# print(y[:10])
 
 RANDOM_STATE = 101
-
-df1 = pd.read_csv(speed_data_aug)
-df2 = pd.read_csv(speed_data_aug)
-
-df1 = df1[df1.road_id == 1]
-# df1 = df1[df1.day_id == 1]
-df1 = df1.drop('road_id', axis=1)
-
-X = df1.iloc[:, 2:3].values.astype(float)
-y = df1.iloc[:, 1:2].values.astype(float)
-
-print(len(X), len(y))
-
 X_train, X_test, y_train, y_test = tts(X, y, test_size=0.2, random_state=RANDOM_STATE)
 X_train, X_val, y_train, y_val = tts(X_train, y_train, test_size=0.2, random_state=RANDOM_STATE)
 
-regressor = svm.SVR(kernel='poly', degree=2, C=50, epsilon=0.02)
-regressor.fit(X_train, y_train)
-accuracy = regressor.score(X_val, y_val)
+svr = svm.SVR()
+svr.fit(X_train, y_train.values.ravel())
+accuracy = svr.score(X_val, y_val)
 print('accuracy = ', accuracy)
 
-plt.scatter(X_val, y_val, color = 'magenta')
-plt.plot(X_val, regressor.predict(X_val), color ='green')
-plt.title('Time vs speed (Support Vector Regression Model)')
-plt.xlabel('speed')
-plt.ylabel('time')
-plt.show()
+# plt.scatter(X_val, y_val, color = 'magenta')
+# plt.plot(X_val, svr.predict(X_val), color ='green')
+# plt.title('Time vs speed (Support Vector Regression Model)')
+# plt.xlabel('speed')
+# plt.ylabel('time')
+# plt.show()
--- temp.py
+++ temp.py
@@ -16,23 +16,26 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('max_colwidth', -1)
 
-speed_data_aug = "speeddata_Aug.csv"
-df1 = pd.read_csv(speed_data_aug)
+custom_data_aug = "customdata_Aug.csv"
+df = pd.read_csv(custom_data_aug)
 
-## Maybe add another column week_of_month or day_of_month
-df = pd.DataFrame(columns=['target_speed', 'speed_10_mins_ago', 'speed_20_mins_ago',
-                            'day_of_week', 'time_of_day', 'route_id', 'avg_speed'])
+df = df.drop('Unnamed: 0', axis=1)
 
-df.route_id = df1.road_id
-df.target_speed = df1.speed
-df.speed_10_mins_ago = df1.speed.shift(1)
-df.speed_20_mins_ago = df1.speed.shift(2)
-df.day_of_week = [(i % 7) + 1 for i in range(len(df))]
-df.time_of_day = [("0" if ((i % 144) // 6) < 10 else "") + str((i % 144) // 6) + ":" +
-                  ("00" if i % 6 == 0 else str(((i % 144) % 6)*10)) for i in range(len(df))]
-df.route_id = df1.road_id
 
-print("before starting")
+# ## Maybe add another column week_of_month or day_of_month
+# df = pd.DataFrame(columns=['target_speed', 'speed_10_mins_ago', 'speed_20_mins_ago',
+#                             'day_of_week', 'time_of_day', 'route_id', 'avg_speed'])
+#
+# df.route_id = df1.road_id
+# df.target_speed = df1.speed
+# df.speed_10_mins_ago = df1.speed.shift(1)
+# df.speed_20_mins_ago = df1.speed.shift(2)
+# df.day_of_week = [(i % 7) + 1 for i in range(len(df))]
+# df.time_of_day = [("0" if ((i % 144) // 6) < 10 else "") + str((i % 144) // 6) + ":" +
+#                   ("00" if i % 6 == 0 else str(((i % 144) % 6)*10)) for i in range(len(df))]
+# df.route_id = df1.road_id
+#
+# print("before starting")
 
 #
 # for times in df['time_of_day'].unique():
@@ -58,19 +61,19 @@ print("before starting")
 # temp = pd.DataFrame(columns=['avg_speed'])
 # temp = pd.read_csv('temp.csv')
 
-for time in df['time_of_day'].unique():
-    for id in df['route_id'].unique():
-        grouped = df.groupby(['time_of_day']).get_group(time).groupby(['route_id']).get_group(id)
-        # print(grouped)
-        n = len(grouped['time_of_day'])
-        mean = grouped['target_speed'].mean()
-        for idx in grouped.index.values:
-            avg_speed = (mean*n - df.loc[idx, 'target_speed'])/(n-1)
-            # print("%d, %d" % (idx, avg_speed))
-            df.loc[idx, 'avg_speed'] = avg_speed
-
-# print(grouped)
-
-df.to_csv('customdata_Aug.csv')
+# for time in df['time_of_day'].unique():
+#     for id in df['route_id'].unique():
+#         grouped = df.groupby(['time_of_day']).get_group(time).groupby(['route_id']).get_group(id)
+#         # print(grouped)
+#         n = len(grouped['time_of_day'])
+#         mean = grouped['target_speed'].mean()
+#         for idx in grouped.index.values:
+#             avg_speed = (mean*n - df.loc[idx, 'target_speed'])/(n-1)
+#             # print("%d, %d" % (idx, avg_speed))
+#             df.loc[idx, 'avg_speed'] = avg_speed
+#
+# # print(grouped)
+#
+# df.to_csv('customdata_Aug.csv')
 
 # print(df.head(10))
